29a7c11c2995ad007a65ee5b063411e5b5b889bf
Akramjon009 <Akramzonabduvahobov6@gmail.com>
2023-12-18 21:53:13 +0500

    info: new leet code midel number 1909

---
+++ leet code 1909/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public bool CanBeIncreasing(int[] nums)
+    {
+        int count = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i - 1] >= nums[i])
+            {
+                count++;
+                if (count > 1)
+                {
+                    return false;
+                }
+
+                if (i > 1 && nums[i - 2] >= nums[i])
+                {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+
+        return true;
+    }
+}
