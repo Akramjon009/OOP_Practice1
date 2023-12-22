1203c546d959663ddf9a8e73d5ced6ba2843c6f5
Akramjon009 <Akramzonabduvahobov6@gmail.com>
2023-12-22 07:48:31 +0500

    info: uyga vazifa 21.12.23

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,17 +1,1 @@
 ﻿
-int size = int.Parse(Console.ReadLine());
-Leetcode stk = new Leetcode(size);
-
-stk.Push(1);                       
-stk.Push(2);                       
-stk.Pop();                         
-stk.Push(2);                       
-stk.Push(3);                       
-stk.Push(4);
-
-stk.Increment(5, 100);          
-stk.Increment(2, 100);          
-stk.Pop();                      
-stk.Pop();                      
-stk.Pop();                      
-stk.Pop();
---
+++ Leet code 1381/Leetcode.cs
@@ -0,0 +1,50 @@
+﻿namespace Leet_code_1381
+{
+    public class Leetcode
+    {
+        private int _maxSize;
+
+        private List<int> list = new List<int>();
+
+        public Leetcode(int maxSize)
+        {
+            _maxSize = maxSize;
+        }
+
+        public void Push(int x)
+        {
+            if (list.Count + 1 > _maxSize)
+            {
+                return;
+            }
+            list.Add(x);
+        }
+
+        public int Pop()
+        {
+            bool isEmpty = !list.Any();
+            if (isEmpty)
+            {
+                return -1;
+            }
+            var last = list.Last();
+            list.RemoveAt(list.Count - 1);
+            return last;
+
+
+        }
+
+        public void Increment(int k, int val)
+        {
+            var i = 0;
+            foreach (int listData in list.ToArray())
+            {
+                if (i < k)
+                {
+                    list[i] = listData + val;
+                }
+                i++;
+            }
+        }
+    }
+}
