90a7b95ab6cd818f753fca3e49568a54c43d17a1
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2023-12-23 12:01:01 +0500

    info: delegate

---
+++ LessonDelegate/Methodelegate.cs
@@ -0,0 +1,22 @@
+﻿namespace LessonDelegate
+{
+    internal class Methodelegate
+    {
+        public int Add(int a, int b) 
+        {
+            return a + b;
+        }
+        public int Remove(int a, int b) 
+        {
+            return a - b; 
+        }
+        public int Devays(int a, int b) 
+        {
+            return a/b;
+        }
+        public int Multiplay(int a, int b) 
+        {
+            return a*b;
+        }
+    }
+}
---
+++ LessonDelegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using LessonDelegate;
+using System.Runtime.InteropServices;
+
+public class Program 
+{
+    public delegate int Mydelegat(int a, int b);
+    static void Main(string[] args) 
+    {
+        int a = 10;
+        int b = 2;
+        Methodelegate methodelegate = new Methodelegate();
+        Mydelegat del = methodelegate.Add;
+        Mydelegat del2 = methodelegate.Devays;
+        Mydelegat del3 = methodelegate.Multiplay;
+        Mydelegat del4 = methodelegate.Devays;
+
+        Console.WriteLine(del(a, b));
+        Console.WriteLine(del2(a, b));
+        Console.WriteLine(del3(a, b));
+        Console.WriteLine(del4(a, b));
+
+
+    }
+}
