3bf7a307241e05a62eec02a060f30f95b499ea53
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2023-12-23 11:04:21 +0500

    info: lesson 13

---
+++ Lesson13.new/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Project 
+{
+    static void Main(string[] args)
+    {
+        Mydelegat newdelegat = Mystring;
+        newdelegat("string");
+    }
+    //delegat funksiyani parqmetr sfatida olishga yordam beradi
+    public delegate void Mydelegat(string str);
+    static void Mystring(string mystr) 
+    {
+        Console.WriteLine(mystr);
+    }
+}
